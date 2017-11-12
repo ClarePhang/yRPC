5eabefea3897d4375150a002cae2f4dd45b2e4aa
zh5202 <zh5202@163.com>
2017-11-12 15:48:02 +0800

    add timming sender on client/server

--- client_test.cpp
+++ client_test.cpp
@@ -1,9 +1,23 @@
 #include <unistd.h>
 #include "socketclient.h"
 
+#include "event.h"
+
+void timingSender(evutil_socket_t fd, short event, void *arg)
+{
+    struct timeval tv = {2,0};
+    struct event *base = (struct event *)arg;
+    
+    printf("\nSend data timming.\n");
+    evtimer_add(base, &tv);
+}
+
 int main(void)
 {
     int ret = -1;
+    struct timeval tv;
+    struct event timeout;
+    struct event_base *base;
     SocketClient client;
     
     //ret = client.connectServer("/tmp/socket_test");
@@ -13,11 +27,16 @@ int main(void)
         printf("connect server failed!\n");
         return -1;
     }
+    
+    /* Initalize the event library */
+    base = event_base_new();
 
-    //while(1)
-    //{
-    //    sleep(5);
-    //}
+    /* Initalize one event */
+    evtimer_assign(&timeout, base, timingSender, (void*) &timeout);
+    evutil_timerclear(&tv);
+    tv.tv_sec = 2;
+    event_add(&timeout, &tv);
+    event_base_dispatch(base);
 
     return 0;
 }
--- server_test.cpp
+++ server_test.cpp
@@ -1,9 +1,23 @@
 #include <unistd.h>
 #include "socketserver.h"
 
+#include "event.h"
+
+void timingSender(evutil_socket_t fd, short event, void *arg)
+{
+    struct timeval tv = {2,0};
+    struct event *base = (struct event *)arg;
+    
+    printf("\nSend data timming.\n");
+    evtimer_add(base, &tv);
+}
+
 int main(void)
 {
-    int ret = -1;
+    int ret = -1;    
+    struct timeval tv;
+    struct event timeout;
+    struct event_base *base;
     SocketServer server;
     
     //ret = server.serverCreate("/tmp/socket_test");
@@ -14,10 +28,15 @@ int main(void)
         return -1;
     }
 
-    while(1)
-    {
-        sleep(5);
-    }
+    /* Initalize the event library */
+	base = event_base_new();
+
+	/* Initalize one event */
+    evtimer_assign(&timeout, base, timingSender, (void*) &timeout);
+	evutil_timerclear(&tv);
+	tv.tv_sec = 2;
+	event_add(&timeout, &tv);
+	event_base_dispatch(base);
 
     return 0;
 }
